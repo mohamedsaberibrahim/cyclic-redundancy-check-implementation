a23e24eddb2b02dc1f6284d9ccc91c8c1a173584
Mohamed Saber <33963542+mohamedsaberibrahim@users.noreply.github.com>
2018-11-22 02:50:41 +0200

    Update and rename main to main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+//Islam
+
+
+
+
+
+
+
+
+//Sidan
+
+
+
+
+
+
+
+
+
+//Saber
+def alter(data,bit):
+    bit-=1
+    mydata = list(data)
+    for i in range (len(mydata)):
+        if i == bit:
+            if(mydata[i] == '0'):
+                mydata[i]='1';
+            elif(mydata[i] == '1'):
+                mydata[i]='0';   
+    return ''.join(mydata)
